9d6e46ef32250bb505f517fbd5e658e90756217b
Adam Luna <adam.luna@outlook.com>
2021-05-09 18:12:13 -0500

    Add attribution to 8.2 and add 8.3

---
+++ week-8/luna_calculator.py
@@ -0,0 +1,23 @@
+# ========================================
+# Title: luna_calculator.py
+# Author: Adam Luna
+# Date: 9 May 2021
+# Description: Create a calculator using Python.
+# ========================================
+
+# Create an “add” function with two parameters and return the total
+def add(add1, add2):
+    return add1 + add2
+
+# Create a “subtract” function with two parameters and return the subtracted total
+def subtract(sub1, sub2):
+    return sub1 - sub2
+
+# Create a "divide" function with two parameters and returnt he divided total
+def divide(div1, div2):
+        return div1 / div2
+
+# print output
+print(add(1, 1))
+print(subtract(4, 4))
+print(divide(6, 3))
--- week-8/luna_hello_world.py
+++ week-8/luna_hello_world.py
@@ -1,1 +1,8 @@
+# ========================================
+# Title: luna_hello_world.py
+# Author: Adam Luna
+# Date: 9 May 2021
+# Description: Print Hello World using Python.
+# ========================================
+
 print('Hello World')
